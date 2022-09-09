30e67d13592647d661ad122fe933a2eac6455ab9
Silve-ster <momanyisilvester31@gmail.com>
2022-09-08 19:47:25 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+* main  - print the string in the put function
+*
+* description - this program prints "Programming is like building a multilingual puzzle, followed by a new line."
+*
+*/
+int main (void)
+{
+	puts("\"Programming is like building a multilingual pwe line");
+	return (0);
+}
